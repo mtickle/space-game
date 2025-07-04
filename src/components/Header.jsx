c6d2823aa801f073ec2d5fc95b51649723f406b1
Mike Tickle <mtickle@gmail.com>
2025-07-04 07:24:07 -0400

    Header Cleanup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = ({ stars, factionFilter, setFactionFilter, starTypeFilter, setSta
             <h1 className="text-4xl font-baumans tracking-wide" style={{ fontFamily: '"Baumans", cursive' }}>StarWeave '78</h1>
             <div className="flex space-x-4">
                 <select
-                    className="p-2 bg-gray-800 text-white rounded"
+                    className="p-1 bg-gray-800 text-white rounded font-semibold text-sm"
                     value={factionFilter}
                     onChange={(e) => setFactionFilter(e.target.value)}
                 >
@@ -23,16 +23,7 @@ const Header = ({ stars, factionFilter, setFactionFilter, starTypeFilter, setSta
                         <option key={faction} value={faction}>{faction}</option>
                     ))}
                 </select>
-                <select
-                    className="p-2 bg-gray-800 text-white rounded"
-                    value={starTypeFilter}
-                    onChange={(e) => setStarTypeFilter(e.target.value)}
-                >
-                    {uniqueStarTypes.map(type => (
-                        <option key={type} value={type}>{type}</option>
-                    ))}
-                </select>
-                <button className="p-2 bg-blue-600 hover:bg-blue-700 rounded text-white">Login</button>
+
             </div>
         </header>
     );
