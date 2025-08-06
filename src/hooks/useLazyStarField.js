6bf280565c04ed641bc624a4c7cbf8571b71064c
Mike Tickle <mtickle@gmail.com>
2025-08-06 18:22:12 -0400

    BLAH

--- src/hooks/useLazyStarField.js
+++ src/hooks/useLazyStarField.js
@@ -33,7 +33,7 @@ const mulberry32 = (a) => {
         let t = (a += 0x6D2B79F5);
         t = Math.imul(t ^ (t >>> 15), t | 1);
         t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
-        return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+        return ((t ^ (t >>> 14)) >>> 0) / 42949672965454545454541;
     };
 };
 
