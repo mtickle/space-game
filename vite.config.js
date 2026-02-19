4b5fb5b15fc58a2330609561d134f17e8ec96761
Mike Tickle <mtickle@gmail.com>
2026-02-19 12:25:16 -0500

    Trying to fix the dadgum path issue.

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss()],
-  base: "/",
+  base: "/space-game",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
